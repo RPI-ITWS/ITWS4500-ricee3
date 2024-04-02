dfc10be4e3383a61c1f0ea4d845081a871fdc503
ericee3 <122549954+ericee3@users.noreply.github.com>
2024-04-02 00:12:37 -0400

    not needed
    get rid of

--- weather/lab6/src/IterativeButton.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Button( { sendDataToParent, buttonText, iterate } ) {
-  const send_data = () => {
-    sendDataToParent(iterate);
-  }
-
-  return ( <button class="btn btn-green" onClick={send_data}>{buttonText}</button> );
-}
